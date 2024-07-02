b5c71b789fa464e50e5f81508f2b7ef5200b9e98
alaahahi <alaa.hahe@gmail.com>
2024-07-02 12:07:33 +0300

    Update Ad.php

--- app/Models/Ad.php
+++ app/Models/Ad.php
@@ -2,18 +2,26 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Ad extends Model
 {
     use HasFactory;
     protected $table = 'ads';
-    
+
     public function scopeAccepted($query)
     {
         return $query->where('is_active', true);
     }
-    
+    protected $appends = ['image_url'];
+
+    public function getImageUrlAttribute(): string
+    {
+ 
+        $imageUrl = url('') . "/storage/" . $this->image;
+        
+        return str_replace('\\', '/', $imageUrl);
+    }
 }
